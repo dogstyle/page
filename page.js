d2fe59ab08c57c2af009a193703c4b0f7337f338
justinkempton <justin@kempton.com>
2013-07-20 15:58:37 -0700

    reduced code fingerprint

--- page.js
+++ page.js
@@ -11,51 +11,49 @@ var PAGE = (function() {
 	var Dog = function Page() { }
 		, interval = undefined
 
-	Dog.prototype.waitLoad = function(group, name, callback) {
-		var limit = 100
-			, count = 0
+	Dog.prototype = {
+		waitLoad : function(group, name, callback) {
+			var limit = 100
+				, count = 0
 
-		if (PAGE[group] && PAGE[group][name]) {
-			return callback(PAGE[group][name])
-		}
-
-		interval = setInterval(function() {
-			if (count > limit || (PAGE[group] && PAGE[group][name])) {
-				if (typeof callback === "function") {
-					callback(PAGE[group][name])
-				}
-				clearInterval(interval)
+			if (PAGE[group] && PAGE[group][name]) {
+				return callback(PAGE[group][name])
 			}
-			count++
-		}, 10)
-	}
 
-	Dog.prototype.addConstructor = function(name, constructor) {
-		if (!this.Constructors) this.Constructors = {}
-		this.Constructors[name] = constructor
-		log("Constructors: " + name + " loaded")
-		return constructor
-	}
-
-	Dog.prototype.addModule = function(name, obj) {
-		if (!this.Modules) this.Modules = {}
-		this.Modules[name] = obj
-		log("module: " + name + " loaded")
-		return obj
-	}
-
-	Dog.prototype.addFunction = function(name, func) {
-		if (!this.Functions) this.Functions = {}
-		this.Functions[name] = func
-		log("function: " + name + " loaded")
-		return func
-	}
-
-	Dog.prototype.addProperty = function(name, value) {
-		if (!this.Properties) this.Properties = {}
-		this.Properties[name] = value
-		log("property: " + name + " loaded")
-		return value
+			interval = setInterval(function() {
+				if (count > limit || (PAGE[group] && PAGE[group][name])) {
+					if (typeof callback === "function") {
+						callback(PAGE[group][name])
+					}
+					clearInterval(interval)
+				}
+				count++
+			}, 10)
+		}
+		, addConstructor : function(name, constructor) {
+			if (!this.Constructors) this.Constructors = {}
+			this.Constructors[name] = constructor
+			log("Constructors: " + name + " loaded")
+			return constructor
+		}
+		, addModule : function(name, obj) {
+			if (!this.Modules) this.Modules = {}
+			this.Modules[name] = obj
+			log("module: " + name + " loaded")
+			return obj
+		}
+		, addFunction : function(name, func) {
+			if (!this.Functions) this.Functions = {}
+			this.Functions[name] = func
+			log("function: " + name + " loaded")
+			return func
+		}
+		, addProperty : function(name, value) {
+			if (!this.Properties) this.Properties = {}
+			this.Properties[name] = value
+			log("property: " + name + " loaded")
+			return value
+		}
 	}
 
 	return new Dog()
